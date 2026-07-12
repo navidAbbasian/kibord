1b26aa3da6ff517fa86dc5510ed86eded5a9ddb2
navidAbbasian <abbasian.navid@gmail.com>
2026-07-12 15:39:00 +0330

    chore: bump version to 0.2.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.navidabbasian.kibord"
         minSdk = 21
         targetSdk = 36
-        versionCode = 11
-        versionName = "0.2.0"
+        versionCode = 12
+        versionName = "0.2.1"
         vectorDrawables {
             useSupportLibrary = true
         }
